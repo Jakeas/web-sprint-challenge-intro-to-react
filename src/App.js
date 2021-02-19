55308d60ca9f6775fa89560d683f3e6ec4e27f69
Jakeas <JakeASierra@gmail.com>
2021-02-19 10:31:48 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [charData, setCharData] = ([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(()=>{
+    const fetchData = () => {
+      axios
+        .get('')
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+    fetchData()
+  },[])
 
   return (
     <div className="App">
