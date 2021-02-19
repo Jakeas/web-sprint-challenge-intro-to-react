8fe16cdcf26b6da92b163e31e642f5fb9cce2af8
Jakeas <JakeASierra@gmail.com>
2021-02-19 12:35:36 -0500

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
+import Char from "./components/Character"
 import './App.css';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [charData, setCharData] = ([])
+  const [charData, setCharData] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(()=>{
     const fetchData = () => {
       axios
-        .get('')
+        .get('https://swapi.dev/api/people/')
         .then((res) => {
           console.log(res)
+          setCharData(res.data)
         })
         .catch((err) => {
           console.log(err)
@@ -25,9 +28,23 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <H11 className="Header">Characters</H11>
+      {
+      charData.map((data, i) => {
+        return <Char key={i} data={data} />
+      })
+      }
+      
     </div>
   );
 }
 
 export default App;
+
+const H11 = styled.h1`
+&:hover{
+    transform: scale(2);
+    transition: all 0.8s ease-in-out;
+}
+transition: all 0.8s ease-in-out;
+`
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,56 @@
 // Write your Character component here
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+function Char (props) {
+  const { data } = props  
+    return (
+    <Container className="container">
+        <H2>{data.name}:</H2>
+        <ParaCat>{"Birth Year: "}</ParaCat>
+        <Para>{data.birth_year}</Para>
+        <ParaCat>{"Gender: "}</ParaCat>
+        <Para>{data.gender}</Para>
+        <ParaCat>{"Mass: "}</ParaCat>
+        <Para>{data.mass}</Para>
+        <ParaCat>{"Height: "}</ParaCat>
+        <Para>{data.height}</Para>
+        {/* <img src={data.homeworld} alt="home planet"/> */}
+
+    </Container>
+    )
+}
+
+export default Char
+
+const Container = styled.div`
+
+`
+
+const H2 = styled.h2`
+font-size: 2rem;
+
+&:hover{
+    transform: scale(0.8);
+    transition: all 0.5s ease-in-out;
+}
+transition: all 0.5s ease-in-out;
+`
+const Para = styled.p`
+font-size: 1.5rem;
+align-items: center;
+margin-left: 3%;
+color:gold;
+
+&:hover{
+    transform: scale(1.5);
+    transition: all 0.3s ease-in-out;
+}
+transition: all 0.3s ease-in-out;
+`
+const ParaCat = styled.p`
+font-size: 1.5rem;
+margin-left: 6%;
+font-weight: bold;
+
+`
